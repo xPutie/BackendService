a1bf57602e0dad2f5f4c20561f8c2a145aba608c
PUTIE\Admin <nguyenmythaihoa05@gmail.com>
2024-10-03 14:15:17 +0700

    ErrorCode modify

--- src/main/java/fu/api/backend/enums/ErrorCode.java
+++ src/main/java/fu/api/backend/enums/ErrorCode.java
@@ -4,12 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum ErrorCode {
     NOT_NULL(1, "Properties can not be null"),
     NOT_EMPTY(2, "Properties can not be empty"),
     INVALID_KEY(3, "Invalid key"),
+    MIN_VALUE(4, "Value must be greater than or equal to {min}"),
+    // ... các mã lỗi khác
     ;
+
     private final int code;
     private final String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    // Getters
 }
+
